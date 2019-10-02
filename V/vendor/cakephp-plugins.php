a1ffd5a9f2c28b268dbdcd4224dcc472a7559854
Ramambason <sylowdev@gmail.com>
2019-10-02 15:16:16 +0300

    moig

---
+++ V/vendor/cakephp-plugins.php
@@ -0,0 +1,14 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'CsvView' => $baseDir . '/vendor/friendsofcake/cakephp-csvview/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Dompdf' => $baseDir . '/vendor/daoandco/cakephp-dompdf/',
+        'Facebook' => $baseDir . '/plugins/Facebook/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'WyriHaximus/TwigView' => $baseDir . '/vendor/wyrihaximus/twig-view/',
+        'dompdf' => $baseDir . '/plugins/dompdf/'
+    ]
+];
